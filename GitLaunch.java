24303079bf32b0be8bbabdf7a6abfc4689b89779
GourabSinha202004 <gourabsinha202004@gmail.com>
2024-02-13 00:08:50 +0530

    Modified after Cloning

--- GitLaunch.java
+++ GitLaunch.java
@@ -4,4 +4,11 @@ class Launch {
 
 		System.out.println("Hello World");
 	}
+
+	puclic static void fun() {
+		int num1 = 10;
+		int num2 = 20;
+		int sum = num1+num2;
+		System.out.println("Sum is: "+sum);
+	}
 }
